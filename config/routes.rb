f8d54520c971180a08943b907efa99db03bf4285
Steve Jorgensen <stevej@stevej.name>
2016-06-05 13:07:07 -0700

    Generate signups and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'signups/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
